259a7f47958e25db1e5503fecccc8cf60cc6def6
Lior S. <79309515+DeveloperCron@users.noreply.github.com>
2025-01-09 18:48:40 +0200

    Added Stack.tsx for easier component placement in component tree

---
+++ src/ui/Stack.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { css } from "@emotion/native";
+import { View, ViewStyle } from "react-native";
+
+type Direction = "row" | "column";
+
+interface StackProps {
+    direction: Direction;
+    children: React.ReactNode;
+    style?: ViewStyle;
+}
+
+export const Stack: React.FC<StackProps> = ({ direction, children, style }) => {
+    const stackStyle = css`
+    width: 100%;
+    display: flex;
+    flex-direction: ${direction};
+  `;
+
+    return <View style={[stackStyle, style]}>{children}</View>;
+};
