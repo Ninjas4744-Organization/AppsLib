f897cd43877a436e4e7347f243fc7d32ba1eb501
Lior S. <79309515+DeveloperCron@users.noreply.github.com>
2025-01-09 20:54:03 +0200

    Made Stack.tsx styleable

--- src/ui/Stack.tsx
+++ src/ui/Stack.tsx
@@ -4,22 +4,20 @@ import { View, ViewStyle } from "react-native";
 
 type Direction = "row" | "column";
 
-interface StackProps
-{
+interface StackProps {
     direction: Direction;
     children: React.ReactNode;
-    style?: ViewStyle;
+    style?: ViewStyle | ViewStyle[];
 }
 
-const Stack: React.FC<StackProps> = ({ direction, children, style }) =>
-{
+const Stack: React.FC<StackProps> = ({ direction, children, style, ...rest }) => {
     const stackStyle = css`
-		width: 100%;
-		display: flex;
-		flex-direction: ${direction};
+        width: 100%;
+        display: flex;
+        flex-direction: ${direction};
     `;
 
-    return <View style={[stackStyle, style]}>{children}</View>;
+    return <View style={[stackStyle, style]} {...rest}>{children}</View>;
 };
 
 export default Stack;
